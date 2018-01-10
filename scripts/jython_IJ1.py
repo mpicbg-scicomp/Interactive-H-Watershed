173b669ae9982e43139e676ac51876adc2cbbf86
benoitlo <benoit.lombardot@gmail.com>
2018-01-10 18:32:44 +0100

    update example script to reflect new parameter allowSplitting

--- scripts/jython_IJ1.py
+++ scripts/jython_IJ1.py
@@ -5,4 +5,5 @@ hMin = 20;
 thresh = 100;
 peakFlooding = 90;
 
-IJ.run("H_Watershed", "impin="+imp.getTitle()+" hmin="+str(hMin)+" thresh="+str(thresh)+" peakflooding="+str(peakFlooding));
+
+IJ.run("H_Watershed", "impin="+imp.getTitle()+" hmin="+str(hMin)+" thresh="+str(thresh)+" peakflooding="+str(peakFlooding)+" outputmask=true allowsplitting=true" );
